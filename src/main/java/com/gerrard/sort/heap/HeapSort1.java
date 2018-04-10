c3bcf1767da01db9329379d620def83d9924cb56
Gerrard-Feng <Gerrard-Feng@users.noreply.github.com>
2018-04-10 23:57:33 +0800

    Complete description for heap sort

--- src/main/java/com/gerrard/sort/heap/HeapSort1.java
+++ src/main/java/com/gerrard/sort/heap/HeapSort1.java
@@ -4,6 +4,13 @@ import com.gerrard.sort.Sort;
 import com.gerrard.util.ArrayHelper;
 import com.gerrard.util.HeapHelper;
 
+/**
+ * Heap-Sort is based on the feature of data structure: heap, with complexity O(n*lgn).
+ * First, build target array as a max-heap.
+ * Secondly, swap first node and last node of heap-array(first node is always the largest).
+ * Then, regard 1-array.length-1 as a new heap-array, maintains its max-heap feature.
+ * Repeat until first node, then the whole array is ordered.
+ */
 public final class HeapSort1 implements Sort {
 
     @Override
