662a1c6561a8af9c063415623d98aa9d637ac2ae
Gerrard-Feng <Gerrard-Feng@users.noreply.github.com>
2018-05-06 23:44:17 +0800

    Add new function for ArrayHelper

--- src/main/java/com/gerrard/util/ArrayHelper.java
+++ src/main/java/com/gerrard/util/ArrayHelper.java
@@ -1,5 +1,7 @@
 package com.gerrard.util;
 
+import java.util.Random;
+
 public final class ArrayHelper {
 
     private ArrayHelper() {
@@ -10,6 +12,26 @@ public final class ArrayHelper {
         int temp = array[i];
         array[i] = array[j];
         array[j] = temp;
+    }
+
+    public static int getMiddle(int left, int right) {
+        return (left + right) >>> 1;
+    }
+
+    public static boolean isSorted(int[] array) {
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] < array[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void shuffle(int[] array) {
+        Random rnd = new Random();
+        for (int i = array.length; i > 1; --i) {
+            ArrayHelper.swap(array, i - 1, rnd.nextInt(i));
+        }
     }
 
     public static void moveForwardBySteps(int[] array, int from, int to, int steps) {
--- src/test/java/com/gerrard/util/ArrayHelperTest.java
+++ src/test/java/com/gerrard/util/ArrayHelperTest.java
@@ -1,7 +1,11 @@
 package com.gerrard.util;
 
+import com.gerrard.helper.ArrayTestHelper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public final class ArrayHelperTest {
 
@@ -15,6 +19,36 @@ public final class ArrayHelperTest {
         ArrayHelper.swap(array, 0, 3);
         int[] expectedArray = {4, 2, 3, 1, 5};
         Assertions.assertArrayEquals(expectedArray, array);
+    }
+
+    @Test
+    void testGetMiddle() {
+        int[] array = {1, 2, 3, 4, 5};
+        int mid = ArrayHelper.getMiddle(0, array.length);
+        Assertions.assertTrue(mid == 2);
+    }
+
+    @Test
+    void testIsSorted() {
+        int[] array1 = {1, 2, 3, 4, 5};
+        Assertions.assertTrue(ArrayHelper.isSorted(array1));
+        int[] array2 = {1, 2, 3, 4, 3};
+        Assertions.assertTrue(!ArrayHelper.isSorted(array2));
+    }
+
+    @Test
+    void testShuffle() {
+        int[] array = ArrayTestHelper.createRandomArray(20, 0, 20);
+        List<Integer> checkList = new LinkedList<>();
+        for (Integer a : array) {
+            checkList.add(a);
+        }
+        ArrayHelper.shuffle(array);
+        for (Integer a : array) {
+            Assertions.assertTrue(checkList.contains(a));
+            checkList.remove(a);
+        }
+        Assertions.assertTrue(checkList.isEmpty());
     }
 
     @Test
