f537623df3444e5ad061dceda601f06554dd4c75
Gerrard-Feng <Gerrard-Feng@users.noreply.github.com>
2018-05-22 00:52:22 +0800

    Refactor for fast-slow-pointer Quick-Sort

--- src/main/java/com/gerrard/sort/compare/quick/QuickSort1.java
+++ src/main/java/com/gerrard/sort/compare/quick/QuickSort1.java
@@ -7,19 +7,19 @@ public class QuickSort1 extends BasicQuickSort {
     @Override
     protected int partition(int[] array, int left, int right) {
         int pivot = array[right];
-        int i = left - 1;
-        for (int j = left; j < right; ++j) {
-            if (array[j] <= pivot) {
+        int slower = left - 1;
+        for (int faster = left; faster < right; ++faster) {
+            if (array[faster] <= pivot) {
                 /*
                  * Slower-pointer always find elements smaller than pivot.
                  * So the final position indicates the number of smaller elements.
                  * And of cause, the smaller elements are all before final position of slower-pointer.
                  */
-                i++;
-                ArrayHelper.swap(array, i, j);
+                slower++;
+                ArrayHelper.swap(array, slower, faster);
             }
         }
-        ArrayHelper.swap(array, i + 1, right);
-        return i + 1;
+        ArrayHelper.swap(array, slower + 1, right);
+        return slower + 1;
     }
 }
